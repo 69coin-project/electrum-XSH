e5decded65eae22b112cc6b6d7e307cad8b9f197
ShieldCoin <ShieldCoin@protonmail.com>
2017-09-27 19:54:08 +0200

    Change Prefix

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -210,7 +210,7 @@ def public_key_to_bc_address(public_key):
     h160 = hash_160(public_key)
     return hash_160_to_bc_address(h160)
 
-def hash_160_to_bc_address(h160, addrtype = 30):
+def hash_160_to_bc_address(h160, addrtype = 63):
     vh160 = chr(addrtype) + h160
     h = Hash(vh160)
     addr = vh160 + h[0:4]
@@ -298,12 +298,12 @@ def PrivKeyToSecret(privkey):
     return privkey[9:9+32]
 
 
-def SecretToASecret(secret, compressed=False, addrtype=30):
+def SecretToASecret(secret, compressed=False, addrtype=63):
     vchIn = chr((addrtype+128)&255) + secret
     if compressed: vchIn += '\01'
     return EncodeBase58Check(vchIn)
 
-def ASecretToSecret(key, addrtype=30):
+def ASecretToSecret(key, addrtype=63):
     vch = DecodeBase58Check(key)
     if vch and vch[0] == chr((addrtype+128)&255):
         return vch[1:]
@@ -358,7 +358,7 @@ def is_address(addr):
         addrtype, h = bc_address_to_hash_160(addr)
     except Exception:
         return False
-    if addrtype not in [30, 33]:
+    if addrtype not in [63, 33]:
         return False
     return addr == hash_160_to_bc_address(h, addrtype)
 
